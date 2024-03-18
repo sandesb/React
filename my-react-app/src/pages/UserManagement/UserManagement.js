d444b5f5ec27b6cc981c02852315c347bc8162a7
sandesb <bajracharyasandeshh@gmail.com>
2024-03-18 08:53:39 +0545

    use Effect

--- my-react-app/src/pages/UserManagement/UserManagement.js
+++ my-react-app/src/pages/UserManagement/UserManagement.js
@@ -7,24 +7,30 @@ const UserManagement = () => {
 
     const data = [
         {
+            id: 1,
             username: 'John',
             email: 'jon@gmail.com',
             age: 25,
             city: 'New York'
         },
         {
+            id: 2,
+
             username: 'Cena',
             email: 'cena@gmail.com',
             age: 30,
             city: 'New Jersey'
         },
         {
+            id: 3,
+
             username: 'Brock',
             email: 'brock@gmail.com',
             age: 22,
             city: 'Old York'
         },
         {
+            id: 4,
             username: 'Lesnar',
             email: 'lesnar@gmail.com',
             age: 52,
@@ -51,6 +57,8 @@ useEffect( ()=> {
           <th class="text-left">Email</th>
           <th>Age</th>
           <th>City</th>
+          <th>Action</th>
+          
         </tr>
 
         {users.length>0 &&
@@ -63,7 +71,17 @@ useEffect( ()=> {
                     <td>{user.email}</td>
                     <td>{user.age}</td>
                     <td>{user.city}</td>
+                    <td>
+                    <Link to={`/pages/AddUser/edit/${user.id}`}>
+            Edit
+            </Link>
 
+            <Link className="red" to={`/pages/AddUser/delete/${user.id}`}>
+            Delete
+            </Link>
+                   
+                   
+                    </td>
                     </tr>
 
                 )
@@ -74,7 +92,7 @@ useEffect( ()=> {
 }
 { users.length === 0 &&
 <tr>
-    <td colSpan={4}>No Records Found.</td></tr>
+    <td colSpan={5}>No Records Found.</td></tr>
 }
         
         </table>
